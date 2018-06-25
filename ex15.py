cc98913eca3f0978d6e30aa955629d6d68387b5c
sumonhlaing <sumonhlaing1997@gmail.com>
2018-06-25 17:28:12 +0630

    reading a file

--- ex15.py
+++ ex15.py
@@ -7,7 +7,7 @@ txt = open(filename)
 print(f"Here's your file {filename}:")
 print(txt.read())
 
-print("Type the filename again:")
+print(" Type the filename again: ")
 file_again = input("> ")
 txt_again = open(file_again)
 
