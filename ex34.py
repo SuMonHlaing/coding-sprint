d289296bd22c4202971ba406c68d8d54b987bd1c
sumonhlaing <sumonhlaing1997@gmail.com>
2018-06-25 17:45:48 +0630

    list exercise

--- ex34.py
+++ ex34.py
@@ -6,11 +6,11 @@ kangaroo = animals[3]
 whale = animals[4]
 platypus = animals[5]
 
-print(f"The first animal is at 0 and is a {bear}")
-print(f"The third animal is at 2 and is a {peacock}")
-print(f"The first animal is {bear}")
-print(f"The animal at 3 is {peacock}")
-print(f"The fifth animal is {whale}")
-print(f"The animal at 2 is {python}")
-print(f"The sixth animal is {platypus}")
-print(f"The animal at 4 is {kangaroo}")
+print(f" The first animal is at 0 and is a {bear}")
+print(f" The third animal is at 2 and is a {peacock}")
+print(f" The first animal is {bear}")
+print(f" The animal at 3 is {peacock}")
+print(f" The fifth animal is {whale}")
+print(f" The animal at 2 is {python}")
+print(f" The sixth animal is {platypus}")
+print(f" The animal at 4 is {kangaroo}")
