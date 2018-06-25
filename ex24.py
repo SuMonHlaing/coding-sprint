91dab4194b854a4f4d709908c0a53264051945f7
sumonhlaing <sumonhlaing1997@gmail.com>
2018-06-25 17:37:09 +0630

    revision

--- ex24.py
+++ ex24.py
@@ -14,9 +14,9 @@ print("----------")
 print(poem)
 print("----------")
 
-
 five = 10 - 2 + 3 - 6
 print(f"This should be five: {five}")
+
 
 def secret_formula(started):
     jelly_beans = started * 500
@@ -28,7 +28,7 @@ def secret_formula(started):
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
-#remember that this is another way to format a string
+# remember that this is another way to format a string
 print("With a starting point of: {}".format(start_point))
 # it's just like with an f"" string
 print(f"We'd have {beans} beans, {jars} jars, and {crates} crates")
