39b5625a777841727c01fecdbf4170f9e03b6903
sumonhlaing <sumonhlaing1997@gmail.com>
2018-06-25 17:47:13 +0630

    learing list

--- ex38.py
+++ ex38.py
@@ -5,7 +5,7 @@ print("Wait there are not 10 things in that list. Let's fix that.")
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Boy"]
 
-while len(stuff) !=10:
+while len(stuff) != 10:
     next_one = more_stuff.pop()
     print("Adding: ", next_one)
     stuff.append(next_one)
@@ -16,7 +16,7 @@ print("There we go: ", stuff)
 print("Let's do some things with stuff.")
 
 print(stuff[1])
-print(stuff[-1]) # whoa! fancy
+print(stuff[-1])  # whoa! fancy
 print(stuff.pop())
-print(' '.join(stuff)) # what? cool!
-print('#'.join(stuff[3:5])) # super stellar!
+print(' '.join(stuff))  # what? cool!
+print('#'.join(stuff[3:5]))  # super stellar!
